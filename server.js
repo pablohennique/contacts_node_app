ca471b1822bffe0c327c5a4675563bb3bc3da470
Pablo Hennique <phennique@gmail.com>
2023-10-03 12:32:22 -0400

    minor change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // MIDDLEWARE
 app.use(express.json()); //Parsing JSON responses
 app.use("/api/contacts", require("./routes/contactRoutes")); //using the root path for api calls
-app.use("/api/users", require("./routes/userRoutes")); //using the root path for api calls
+app.use("/api/users", require("./routes/userRoutes")); //using the root path for all api calls
 app.use(errorHandler); //Custom build error handling middleware
 
 app.listen(port, () => {
